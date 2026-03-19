8f988afb6e23ad1c4363e32eb5b441afb44d008b
VokalTuna <>
2026-03-19 18:34:33 +0100

    Made my version more aligned with instructor solution.

--- main.go
+++ main.go
@@ -7,16 +7,22 @@ import (
 	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
 	filepathRoot := "."
 	const port = "8080"
-	apiCfg := &apiConfig{}
+	apiCfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
-	mux.HandleFunc("/healthz", handlerHealthz)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /healthz", handlerReadiness)
 
 	srv := &http.Server{
 		Handler: mux,
@@ -27,28 +33,11 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func handlerHealthz(rw http.ResponseWriter, req *http.Request) {
-	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	rw.WriteHeader(http.StatusOK)
-	rw.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
 func (cfg *apiConfig) handlerMetrics(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	rw.WriteHeader((http.StatusOK))
 	hits := cfg.fileserverHits.Load()
 	rw.Write([]byte("Hits: " + strconv.Itoa(int(hits))))
-}
-
-func (cfg *apiConfig) handlerReset(rw http.ResponseWriter, req *http.Request) {
-	cfg.fileserverHits.Store(0)
-	hits := cfg.fileserverHits.Load()
-	rw.WriteHeader((http.StatusOK))
-	rw.Write([]byte("Hits: " + strconv.Itoa(int(hits))))
-}
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
---
+++ reset.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerReset(rw http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	rw.WriteHeader((http.StatusOK))
+	rw.Write([]byte("Hits reset to 0"))
+}
