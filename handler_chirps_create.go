74d2077524d9baf97091f2593d4332264a8ac616
VokalTuna <knutolavs@pm.me>
2026-03-28 15:54:41 +0100

    Can now create a post and store it

---
+++ handler_chirps_create.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/VokalTuna/chirpy/internal/database"
+	"github.com/google/uuid"
+)
+
+type Chirp struct {
+	ID         uuid.UUID `json:"id"`
+	Created_at time.Time `json:"created_at"`
+	Updated_at time.Time `json:"updated_at"`
+	Body       string    `json:"body"`
+	User_id    uuid.UUID `json:"user_id"`
+}
+
+func (cfg *apiConfig) handlerChirpsCreate(rw http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Body    string    `json:"body"`
+		User_id uuid.UUID `json:"user_id"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(rw, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	chirpMsg, err := validateChirp(params.Body)
+
+	if err != nil {
+		respondWithError(rw, http.StatusBadRequest, err.Error(), err)
+		return
+	}
+
+	chirp, err := cfg.db.CreateChirp(context.Background(), database.CreateChirpParams{
+		Body:   chirpMsg,
+		UserID: params.User_id,
+	})
+
+	respondWithJSON(rw, http.StatusCreated, Chirp{
+		ID:         chirp.ID,
+		Created_at: chirp.CreatedAt,
+		Updated_at: chirp.UpdatedAt,
+		Body:       chirp.Body,
+		User_id:    chirp.UserID,
+	})
+}
+
+func validateChirp(body string) (string, error) {
+	const maxChirpLength = 140
+	if len(body) > maxChirpLength {
+		return "", errors.New("Chirp is too long")
+	}
+
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+
+	output := getCleanBody(body, badWords)
+	return output, nil
+}
+
+func getCleanBody(msg string, badWords map[string]struct{}) string {
+	words := strings.Split(msg, " ")
+	for i, word := range words {
+		lowerWord := strings.ToLower(word)
+		if _, ok := badWords[lowerWord]; ok {
+			words[i] = "****"
+		}
+	}
+
+	return strings.Join(words, " ")
+}
