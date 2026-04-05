cc8a5e4cb8c633a113a70006c0cb9e118c3ed06d
VokalTuna <knutolavs@pm.me>
2026-04-05 18:38:41 +0200

    Made queries related to refresh tokens, and added the refresh token in login function

--- handler_user_login.go
+++ handler_user_login.go
@@ -6,13 +6,13 @@ import (
 	"time"
 
 	"github.com/VokalTuna/chirpy/internal/auth"
+	"github.com/VokalTuna/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) handlerUserLogin(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Email            string `json:"email"`
-		Password         string `json:"password"`
-		ExpiresInSeconds int    `json:"expires_in_seconds"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
 	type response struct {
 		User
@@ -38,17 +38,17 @@ func (cfg *apiConfig) handlerUserLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	expiresInSeconds := time.Hour
-
-	if param.ExpiresInSeconds > 0 && param.ExpiresInSeconds < 3600 {
-		expiresInSeconds = time.Duration(param.ExpiresInSeconds) * time.Second
-	}
-
-	token, err := auth.MakeJWT(dbUser.ID, cfg.secret, expiresInSeconds)
+	token, err := auth.MakeJWT(dbUser.ID, cfg.secret, time.Hour)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't create access JWT", err)
 		return
 	}
+
+	refreshToken, err := cfg.db.CreateRefreshToken(r.Context(), database.CreateRefreshTokenParams{
+		Token:     auth.MakeRefreshToken(),
+		UserID:    dbUser.ID,
+		ExpiresAt: time.Now().UTC().Add(time.Hour * 24 * 60),
+	})
 
 	respondWithJSON(w, http.StatusOK, response{
 		User: User{
@@ -57,6 +57,7 @@ func (cfg *apiConfig) handlerUserLogin(w http.ResponseWriter, r *http.Request) {
 			UpdatedAt: dbUser.UpdatedAt,
 			Email:     dbUser.Email,
 		},
-		Token: token,
+		Token:        token,
+		RefreshToken: refreshToken.Token,
 	})
 }
