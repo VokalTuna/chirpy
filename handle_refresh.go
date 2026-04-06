af1b19ae8b5f524dee9faf5d264c41a122eb61f9
VokalTuna <knutolavs@pm.me>
2026-04-06 16:56:29 +0200

    Able to check if a refresh token is avaialable and theres an user. Able to revoke refresh token.

---
+++ handle_refresh.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/VokalTuna/chirpy/internal/auth"
+)
+
+func (cfg *apiConfig) handleRefresh(w http.ResponseWriter, r *http.Request) {
+	type Response struct {
+		Token string `json:"token"`
+	}
+	tokenString, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "No valid strings", err)
+		return
+	}
+	dbUser, err := cfg.db.GetUserFromRefreshToken(r.Context(), tokenString)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "No valid strings", err)
+		return
+	}
+	accessToken, err := auth.MakeJWT(dbUser.ID, cfg.secret, time.Hour)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Not able to create access token,", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, Response{
+		Token: accessToken,
+	})
+}
+
+func (cfg *apiConfig) handleRevoke(w http.ResponseWriter, r *http.Request) {
+	tokenString, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "No valid token", err)
+		return
+	}
+	_, err = cfg.db.RevokeRefreshToken(r.Context(), tokenString)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "No valid token", err)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
--- main.go
+++ main.go
@@ -58,6 +58,8 @@ func main() {
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 	mux.HandleFunc("POST /api/login", apiCfg.handlerUserLogin)
+	mux.HandleFunc("POST /api/refresh", apiCfg.handleRefresh)
+	mux.HandleFunc("POST /api/revoke", apiCfg.handleRevoke)
 
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerGetChirps)
